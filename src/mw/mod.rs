1b2074ac907dc4a58ef2526b7421d29e77585740
Daniel Arany <50270724+Goldan32@users.noreply.github.com>
2025-01-22 10:06:32 +0100

    Core: FrontEndOutput (#12)
    Make output a trait as well

--- src/mw/mod.rs
+++ src/mw/mod.rs
@@ -4,7 +4,7 @@ pub mod ui;
 
 use crate::mw::{
     db::DatabaseOps,
-    ui::{FrontEndInput, InputCommand},
+    ui::{FrontEndInput, FrontEndOutput, InputCommand, TaskDisplay},
 };
 
 pub struct Middleware<T: FrontEndInput, U: DatabaseOps> {
@@ -12,7 +12,7 @@ pub struct Middleware<T: FrontEndInput, U: DatabaseOps> {
     db: U,
 }
 
-impl<T: FrontEndInput, U: DatabaseOps> Middleware<T, U> {
+impl<T: FrontEndInput + FrontEndOutput, U: DatabaseOps> Middleware<T, U> {
     pub fn new() -> Self {
         Self {
             ui: T::new(),
@@ -24,24 +24,23 @@ impl<T: FrontEndInput, U: DatabaseOps> Middleware<T, U> {
         let command: InputCommand = self.ui.execute();
         match command {
             InputCommand::Add(t) => {
-                let inserted_task = self
+                let _inserted_task = self
                     .db
                     .insert_or_modify(t)
                     .expect("Failed insert_or_modify operation");
-                println!("{}", inserted_task);
             }
             InputCommand::Ls(s) => {
                 let v = self.db.list(s).expect("Failed list operation");
                 for t in v {
-                    t.display_oneline();
+                    self.ui.display_task(t, TaskDisplay::Oneline);
                 }
             }
             InputCommand::Show(id) => {
                 let t = self.db.get_by_id(id).unwrap();
-                println!("{}", t);
+                self.ui.display_task(t, TaskDisplay::Full);
             }
             #[allow(unreachable_patterns)]
-            _ => println!("Not implemented yet"),
+            _ => eprintln!("Not implemented yet"),
         }
     }
 }
