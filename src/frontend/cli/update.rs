879621b074723cae2e26f20876d999ca9b6da82f
Goldan32 <adani32@gmail.com>
2025-05-24 19:21:51 +0200

    Possible to update title

--- src/frontend/cli/update.rs
+++ src/frontend/cli/update.rs
@@ -4,15 +4,14 @@ use std::convert::TryInto;
 #[derive(Debug, PartialEq)]
 pub struct Update {
     pub id: String,
-    pub field: String,
-    pub new_value: String,
+    pub new_title: String,
 }
 
 impl TryInto<InputCommand> for Update {
     type Error = FrontEndError;
     fn try_into(self) -> Result<InputCommand, Self::Error> {
         match self.id.parse::<u64>() {
-            Ok(id) => Ok(InputCommand::Update(id)),
+            Ok(id) => Ok(InputCommand::Update(id, self.new_title)),
             Err(_) => Err(FrontEndError::ParseError("id from cli".to_string())),
         }
     }
