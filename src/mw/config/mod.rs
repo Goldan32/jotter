f2a981cf2ae1eeeadc358d06af95dfc42883648d
Goldan32 <50270724+Goldan32@users.noreply.github.com>
2025-08-10 13:40:36 +0200

    Recreate current state with config-rs

--- src/mw/config/mod.rs
+++ src/mw/config/mod.rs
@@ -1,13 +1,14 @@
-use dotenv::dotenv;
-use rand::distr::{Alphanumeric, SampleString};
+use config::{Config, ConfigError, Environment, File};
+use serde_derive::Deserialize;
 use std::{
-    fs,
     path::PathBuf,
     sync::{Arc, OnceLock},
 };
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
 pub struct AppConfig {
+    #[allow(unused)]
+    pub editor: PathBuf,
     #[allow(unused)]
     pub root_dir: PathBuf,
     pub task_db: PathBuf,
@@ -22,62 +23,31 @@ impl AppConfig {
     }
 
     pub fn init() {
-        dotenv().ok();
-        let root_dir: PathBuf = if let Ok(var) = std::env::var("BJL_ROOT") {
-            var.into()
-        } else if let Some(dir) = dirs::data_local_dir() {
-            let mut root_dir = dir;
-            root_dir.push("bjl");
-            root_dir
-        } else {
-            // Try to not collide with other users
-            let unique_string = Alphanumeric.sample_string(&mut rand::rng(), 6);
-            // Assume windows #justworks
-            let mut root_dir = PathBuf::from(r"/var/lib/bjl");
-            root_dir.push(unique_string);
-            root_dir
-        };
+        let s = Config::builder()
+            .add_source(File::with_name("example-config.toml"))
+            .add_source(Environment::with_prefix("bjl"))
+            .build()
+            .unwrap();
 
-        let work_dir: PathBuf = if let Ok(var) = std::env::var("BJL_CACHE") {
-            var.into()
-        } else if let Some(dir) = dirs::cache_dir() {
-            let mut work_dir = dir;
-            work_dir.push("bjl");
-            work_dir
-        } else {
-            let mut work_dir = PathBuf::from(r"/var/cache/bjl");
-            let unique_string = Alphanumeric.sample_string(&mut rand::rng(), 6);
-            work_dir.push(unique_string);
-            work_dir
-        };
-
-        let task_db: PathBuf = if let Ok(var) = std::env::var("BJL_DATABASE") {
-            let tmp: PathBuf = var.into();
-            let parent = tmp.parent().unwrap();
-            fs::create_dir_all(&parent).unwrap();
-            tmp
-        } else {
-            let mut tmp = root_dir.clone();
-            tmp.push("production.db3");
-            tmp
-        };
-
-        fs::create_dir_all(&root_dir).unwrap();
-        fs::create_dir_all(&work_dir).unwrap();
-
+        println!("{:?}", s);
         Self::global()
-            .set(Arc::new(Self {
-                root_dir,
-                task_db,
-                work_dir,
-            }))
-            .expect("Already initialized");
+            .set(Arc::new(s.try_deserialize().unwrap()))
+            .expect("Already init");
     }
 
     pub fn get() -> Arc<AppConfig> {
-        Self::global()
-            .get()
-            .expect("Config not initialized")
-            .clone()
+        Self::global().get().expect("Config not init").clone()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_config_init_and_get() {
+        AppConfig::init();
+        let cfg = AppConfig::get();
+        assert_eq!(cfg.task_db, PathBuf::from(r"task_db_path"));
     }
 }
